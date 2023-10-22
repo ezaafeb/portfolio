63d1083bb990ede98ff0f7f3cdd1cfac9a79edc2
Reza Febriansyah <136249860+ezaafeb@users.noreply.github.com>
2023-10-22 00:19:13 -0500

    commit

---
+++ src/Details.js
@@ -0,0 +1,169 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "ezaafebri",
+  tagline: "I am for Cyber Security",
+  img: profile,
+  about: `Hi! Saya seorang pengembang web anakwhitehat.com dan pendiri dari sibershop.store saya seorang hacktivist didunia cybersecurity.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://www.github.com/ezaafeb",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/ezaafebri",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Founder",
+    Company: `Siber Shop Store`,
+    Location: "Bekasi",
+    Type: "Full Time",
+    Duration: "January 2023",
+  },
+  {
+    Position: "Freelance",
+    Company: `anakwhitehat.com`,
+    Location: "Bekasi",
+    Type: "Full Time",
+    Duration: "2019 - Now",
+  },
+  {
+    Position: "Operator",
+    Company: `PT. Karya Bahana Unigam Plant 2`,
+    Location: "Bekasi",
+    Type: "Internship",
+    Duration: "2023 - 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Otodidak",
+    Company: "GIthub, YouTube, Google,",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2019 - Present",
+  },
+  {
+    Position: "SMKN 2 CIKARANG BARAT",
+    Company: `Teknik Komputer Jaringan`,
+    Location: "Cikarang",
+    Type: "Pelajar Smk",
+    Duration: "lulusan 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "anakwhitehat.com",
+    image: projectImage1,
+    description: `Situs ini berisi tutorial keamanan cyber, Android dan system operasi windows dan linux hingga berita update tentang cybersecurity`,
+    techstack: "Cyber Security",
+    previewLink: "https://anakwhitehat.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Siber Shop Store",
+    image: projectImage2,
+    description: `Siber Shop adalah perusahaan rintisan yang bergerak dibidang IT e-commerce produk digital dan buku anak IT dan jasa layanan digital.`,
+    techstack: "Startup",
+    previewLink: "https://sibershop.store",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Anoncyberteam",
+    image: projectImage3,
+    description: `Anoncyberteam adalah sebuah komunitas IT Security berdiri tahun 2015 dan telah tersebar diseluruh wilayah di indonesia founder by MR.Tenwap.`,
+    techstack: "Komunitas",
+    previewLink: "https://anoncyberteam.or.id",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "OSINT Penipuan",
+    image: projectImage4,
+    description: `OSINT (Open Source Intelligence) adalah proses pengumpulan, analisis, dan interpretasi informasi yang bersumber dari sumber-sumber publik dan terbuka untuk mendapatkan pemahaman yang lebih baik tentang suatu subjek atau topik tertentu. Dalam konteks keamanan informasi, intelijen, atau investigasi.`,
+    techstack: "Blog",
+    previewLink: "https://www.anakwhitehat.com/2023/08/osint-modus-penipuan-bekedok-joki-pinjol.html",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Introduction Ethical Hacking",
+    image: projectImage5,
+    description: `Ethical hacking adalah praktik keamanan komputer di mana seorang profesional keamanan (ethical hacker) secara sah dan etis mencoba mengevaluasi, mengidentifikasi, menguji, dan mengatasi kerentanan keamanan dalam sistem, jaringan, aplikasi, atau infrastruktur komputer lainnya. `,
+    techstack: "Blog",
+    previewLink: "https://www.anakwhitehat.com/2023/07/blog-post.html",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Social Engineering",
+    image: projectImage6,
+    description: `Social engineering adalah teknik manipulasi psikologis yang digunakan oleh peretas atau penipu untuk memanipulasi orang lain agar mengungkapkan informasi rahasia, memberikan akses ke sistem atau jaringan, atau melakukan tindakan yang menguntungkan bagi peretas. `,
+    techstack: "Blog",
+    previewLink: "https://www.anakwhitehat.com/2023/07/apa-itu-social-engineering.html",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "ezaafebri@gmail.com",
+  phone: "+62 815xxxx2945",
+};
